e5831c8fae762643a06fa831c02fb785bb9dab1e
salik <saliktariq@icloud.com>
2023-03-28 11:27:30 +0100

    Create input_output_percentages.py

---
+++ input_output_percentages.py
@@ -0,0 +1,14 @@
+# Given an input as a percentage, write a program which converts the percentage into decimal form and outputs the result.
+#
+# Ex: If the input is:
+#
+# 54
+# the output is
+#
+# 0.54
+
+percentage = float(input())
+
+decimal = percentage / 100
+
+print(decimal)
