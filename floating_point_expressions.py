6964a32ab96b877d4129fb0977836c3943d7b90b
salik <saliktariq@icloud.com>
2023-03-28 09:56:19 +0100

    Create floating_point_expressions.py

---
+++ floating_point_expressions.py
@@ -0,0 +1,11 @@
+# Write an assignment statement for the following mathematical equation:
+# y = (1/3)x + (x/4) + 2x
+# Keep x as an integer.
+# Use an expression that matches the equation's right side as closely as possible.
+# If the input is -1, the output is -2.58333.
+
+x = int(input())
+
+y = ((1.0 / 3.0) * x) + (x / 4.0) + (2.0 * x)
+
+print(y)
