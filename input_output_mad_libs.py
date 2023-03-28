07b52441329cc6ecda3ce04b62d2b8dc764f1472
salik <saliktariq@icloud.com>
2023-03-28 11:26:41 +0100

    Create input_output_mad_libs.py

---
+++ input_output_mad_libs.py
@@ -0,0 +1,9 @@
+# Mad Libs are word games which require a person to provide various words to complete a short and fun story.
+# Complete the following program to make a Mad Lib of your own.
+
+name = input()
+location = input()
+number = int(input())
+plural_noun = input()
+
+print(name + " went to " + location + " to buy " + str(number) + " different types of " + plural_noun + ".")
